c43eb0f9a09ece8ba8fd3d2ebd5b5d5969798403
励志买套上海苏河湾大平层 <94773058+chenjjiaa@users.noreply.github.com>
2022-10-13 11:35:02 +0800

    Update WebUtils.java

--- src/main/java/javax/servlet/utils/WebUtils.java
+++ src/main/java/javax/servlet/utils/WebUtils.java
@@ -12,10 +12,10 @@ import java.util.Enumeration;
  * Blog：    https://juejin.cn/user/1856417285289304/posts
  * Github：  https://github.com/chenjjiaa
  */
-public class WebUtils {
+public class WebUtil {
     /**
      * 作用：将前端传入的参数封装成一个对象然后返回到 Controller
-     * @param request HttpServletRequest，内部包含了请求头带来的所有信息、参数等
+     * @param request：HttpServletRequest，内部包含了请求头带来的所有信息、参数等
      * @param object 要返回的对象类型，通过反射构造此对象的属性
      * @return
      */
@@ -23,22 +23,15 @@ public class WebUtils {
                                              Object object) {
         Enumeration fieldNames = request.getParameterNames();
         while (fieldNames.hasMoreElements()) {
-            //获取类
-            Class c = object.getClass();
-            //获取属性名
-            String fieldName = (String) fieldNames.nextElement();
-            //拼串，拼方法名
-            String setMethodName = "set" + fieldName.toUpperCase().charAt(0) + fieldName.substring(1);
+            Class c = object.getClass(); // 获取类
+            String fieldName = (String) fieldNames.nextElement(); // 获取属性名
+            String setMethodName = "set" + fieldName.toUpperCase().charAt(0) + fieldName.substring(1); // 得到方法名
             try {
-                //获取要调用的方法对象
                 Method setMethod = c.getDeclaredMethod(setMethodName, String.class);
-                //用方法对象来调用方法，实现对象属性赋值
-                setMethod.invoke(object,request.getParameter(fieldName));
+                setMethod.invoke(object,request.getParameter(fieldName)); // 调用方法，实现对象属性赋值
             } catch (Exception e) {
-                /**
-                 * 有些对象没有那个参数，就不必理会，不用抛出异常
-                 */
-                //e.printStackTrace();
+                // 有些对象没有某些参数，就不必理会 不用抛出异常
+                
             }
         }
         return object;
